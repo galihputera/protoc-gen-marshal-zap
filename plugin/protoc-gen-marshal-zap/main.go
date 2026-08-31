1eb58f291ceeddfe8974a78191f64c24580950fa
Galih Putera <galih.suminto@xendit.co>
2026-08-31 11:13:29 +0700

    feat: implement well-known types JSON format

--- plugin/protoc-gen-marshal-zap/main.go
+++ plugin/protoc-gen-marshal-zap/main.go
@@ -14,7 +14,124 @@ import (
 const (
 	zapcorePkg = protogen.GoImportPath("go.uber.org/zap/zapcore")
 	fmtPkg     = protogen.GoImportPath("fmt")
+	stringsPkg = protogen.GoImportPath("strings")
 )
+
+// wellKnownType represents the category of a well-known protobuf type
+type wellKnownType int
+
+const (
+	notWellKnown wellKnownType = iota
+	wktTimestamp
+	wktDuration
+	wktStruct
+	wktValue
+	wktListValue
+	wktBoolValue
+	wktStringValue
+	wktBytesValue
+	wktInt32Value
+	wktInt64Value
+	wktUInt32Value
+	wktUInt64Value
+	wktFloatValue
+	wktDoubleValue
+	wktAny
+	wktFieldMask
+	wktEmpty
+)
+
+// getWellKnownType returns the well-known type category for a message field
+func getWellKnownType(f *protogen.Field) wellKnownType {
+	if f.Desc.Kind() != protoreflect.MessageKind {
+		return notWellKnown
+	}
+	fullName := string(f.Desc.Message().FullName())
+	switch fullName {
+	case "google.protobuf.Timestamp":
+		return wktTimestamp
+	case "google.protobuf.Duration":
+		return wktDuration
+	case "google.protobuf.Struct":
+		return wktStruct
+	case "google.protobuf.Value":
+		return wktValue
+	case "google.protobuf.ListValue":
+		return wktListValue
+	case "google.protobuf.BoolValue":
+		return wktBoolValue
+	case "google.protobuf.StringValue":
+		return wktStringValue
+	case "google.protobuf.BytesValue":
+		return wktBytesValue
+	case "google.protobuf.Int32Value":
+		return wktInt32Value
+	case "google.protobuf.Int64Value":
+		return wktInt64Value
+	case "google.protobuf.UInt32Value":
+		return wktUInt32Value
+	case "google.protobuf.UInt64Value":
+		return wktUInt64Value
+	case "google.protobuf.FloatValue":
+		return wktFloatValue
+	case "google.protobuf.DoubleValue":
+		return wktDoubleValue
+	case "google.protobuf.Any":
+		return wktAny
+	case "google.protobuf.FieldMask":
+		return wktFieldMask
+	case "google.protobuf.Empty":
+		return wktEmpty
+	default:
+		return notWellKnown
+	}
+}
+
+// getWellKnownTypeFromMapValue returns the well-known type category for a map value
+func getWellKnownTypeFromMapValue(f *protogen.Field) wellKnownType {
+	if f.Desc.MapValue().Kind() != protoreflect.MessageKind {
+		return notWellKnown
+	}
+	fullName := string(f.Desc.MapValue().Message().FullName())
+	switch fullName {
+	case "google.protobuf.Timestamp":
+		return wktTimestamp
+	case "google.protobuf.Duration":
+		return wktDuration
+	case "google.protobuf.Struct":
+		return wktStruct
+	case "google.protobuf.Value":
+		return wktValue
+	case "google.protobuf.ListValue":
+		return wktListValue
+	case "google.protobuf.BoolValue":
+		return wktBoolValue
+	case "google.protobuf.StringValue":
+		return wktStringValue
+	case "google.protobuf.BytesValue":
+		return wktBytesValue
+	case "google.protobuf.Int32Value":
+		return wktInt32Value
+	case "google.protobuf.Int64Value":
+		return wktInt64Value
+	case "google.protobuf.UInt32Value":
+		return wktUInt32Value
+	case "google.protobuf.UInt64Value":
+		return wktUInt64Value
+	case "google.protobuf.FloatValue":
+		return wktFloatValue
+	case "google.protobuf.DoubleValue":
+		return wktDoubleValue
+	case "google.protobuf.Any":
+		return wktAny
+	case "google.protobuf.FieldMask":
+		return wktFieldMask
+	case "google.protobuf.Empty":
+		return wktEmpty
+	default:
+		return notWellKnown
+	}
+}
 
 func generateListField(g *protogen.GeneratedFile, f *protogen.Field) {
 	fname := f.Desc.Name()
@@ -42,11 +159,7 @@ func generateListField(g *protogen.GeneratedFile, f *protogen.Field) {
 	case protoreflect.GroupKind:
 		g.P("enc.AppendReflected(v)")
 	case protoreflect.MessageKind:
-		g.P("if obj, ok := interface{}(v).(", g.QualifiedGoIdent(zapcorePkg.Ident("ObjectMarshaler")), "); ok {")
-		g.P("enc.AppendObject(obj)")
-		g.P("} else {")
-		g.P("enc.AppendReflected(v)")
-		g.P("}")
+		generateListWellKnownOrMessage(g, f)
 	case protoreflect.StringKind:
 		g.P("enc.AppendString(v)")
 	default:
@@ -56,6 +169,102 @@ func generateListField(g *protogen.GeneratedFile, f *protogen.Field) {
 	g.P("return nil")
 	g.P("}")
 	g.P("enc.AddArray(\"", fname, "\",", g.QualifiedGoIdent(zapcorePkg.Ident("ArrayMarshalerFunc")), "(", fname, "ArrMarshaller))")
+}
+
+// generateListWellKnownOrMessage generates array append code for message kinds,
+// handling well-known types with their JSON representation
+func generateListWellKnownOrMessage(g *protogen.GeneratedFile, f *protogen.Field) {
+	wkt := getWellKnownType(f)
+	switch wkt {
+	case wktTimestamp:
+		// Timestamp -> RFC 3339 string
+		g.P("if v != nil {")
+		g.P("enc.AppendString(v.AsTime().Format(\"2006-01-02T15:04:05.999999999Z07:00\"))")
+		g.P("} else {")
+		g.P("enc.AppendReflected(nil)")
+		g.P("}")
+	case wktDuration:
+		// Duration -> string like "1.5s"
+		g.P("if v != nil {")
+		g.P("enc.AppendString(v.AsDuration().String())")
+		g.P("} else {")
+		g.P("enc.AppendReflected(nil)")
+		g.P("}")
+	case wktStruct, wktValue, wktListValue, wktAny:
+		// Use AddReflected for complex types
+		g.P("enc.AppendReflected(v)")
+	case wktFieldMask:
+		// FieldMask -> comma-separated paths
+		g.P("if v != nil {")
+		g.P("enc.AppendString(", g.QualifiedGoIdent(stringsPkg.Ident("Join")), "(v.GetPaths(), \",\"))")
+		g.P("} else {")
+		g.P("enc.AppendReflected(nil)")
+		g.P("}")
+	case wktEmpty:
+		// Empty -> empty object representation
+		g.P("enc.AppendReflected(struct{}{})")
+	case wktBoolValue:
+		g.P("if v != nil {")
+		g.P("enc.AppendBool(v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AppendReflected(nil)")
+		g.P("}")
+	case wktStringValue:
+		g.P("if v != nil {")
+		g.P("enc.AppendString(v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AppendReflected(nil)")
+		g.P("}")
+	case wktBytesValue:
+		g.P("if v != nil {")
+		g.P("enc.AppendByteString(v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AppendReflected(nil)")
+		g.P("}")
+	case wktInt32Value:
+		g.P("if v != nil {")
+		g.P("enc.AppendInt32(v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AppendReflected(nil)")
+		g.P("}")
+	case wktInt64Value:
+		g.P("if v != nil {")
+		g.P("enc.AppendInt64(v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AppendReflected(nil)")
+		g.P("}")
+	case wktUInt32Value:
+		g.P("if v != nil {")
+		g.P("enc.AppendUint32(v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AppendReflected(nil)")
+		g.P("}")
+	case wktUInt64Value:
+		g.P("if v != nil {")
+		g.P("enc.AppendUint64(v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AppendReflected(nil)")
+		g.P("}")
+	case wktFloatValue:
+		g.P("if v != nil {")
+		g.P("enc.AppendFloat32(v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AppendReflected(nil)")
+		g.P("}")
+	case wktDoubleValue:
+		g.P("if v != nil {")
+		g.P("enc.AppendFloat64(v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AppendReflected(nil)")
+		g.P("}")
+	default:
+		// Regular message - check for ObjectMarshaler interface
+		g.P("if obj, ok := interface{}(v).(", g.QualifiedGoIdent(zapcorePkg.Ident("ObjectMarshaler")), "); ok {")
+		g.P("enc.AppendObject(obj)")
+		g.P("} else {")
+		g.P("enc.AppendReflected(v)")
+		g.P("}")
+	}
 }
 
 func generateMapField(g *protogen.GeneratedFile, f *protogen.Field) {
@@ -84,11 +293,7 @@ func generateMapField(g *protogen.GeneratedFile, f *protogen.Field) {
 	case protoreflect.GroupKind:
 		g.P("enc.AddReflected(", g.QualifiedGoIdent(fmtPkg.Ident("Sprintf")), "(\"%v\", k), v)")
 	case protoreflect.MessageKind:
-		g.P("if obj, ok := interface{}(v).(", g.QualifiedGoIdent(zapcorePkg.Ident("ObjectMarshaler")), "); ok {")
-		g.P("enc.AddObject(", g.QualifiedGoIdent(fmtPkg.Ident("Sprintf")), "(\"%v\", k), obj)")
-		g.P("} else {")
-		g.P("enc.AddReflected(", g.QualifiedGoIdent(fmtPkg.Ident("Sprintf")), "(\"%v\", k), v)")
-		g.P("}")
+		generateMapWellKnownOrMessage(g, f)
 	case protoreflect.StringKind:
 		g.P("enc.AddString(", g.QualifiedGoIdent(fmtPkg.Ident("Sprintf")), "(\"%v\", k), v)")
 	default:
@@ -97,6 +302,103 @@ func generateMapField(g *protogen.GeneratedFile, f *protogen.Field) {
 	g.P("}")
 	g.P("return nil")
 	g.P("}))")
+}
+
+// generateMapWellKnownOrMessage generates map value encoding for message kinds,
+// handling well-known types with their JSON representation
+func generateMapWellKnownOrMessage(g *protogen.GeneratedFile, f *protogen.Field) {
+	wkt := getWellKnownTypeFromMapValue(f)
+	keyFmt := g.QualifiedGoIdent(fmtPkg.Ident("Sprintf")) + "(\"%v\", k)"
+	switch wkt {
+	case wktTimestamp:
+		// Timestamp -> RFC 3339 string
+		g.P("if v != nil {")
+		g.P("enc.AddString(", keyFmt, ", v.AsTime().Format(\"2006-01-02T15:04:05.999999999Z07:00\"))")
+		g.P("} else {")
+		g.P("enc.AddReflected(", keyFmt, ", nil)")
+		g.P("}")
+	case wktDuration:
+		// Duration -> string like "1.5s"
+		g.P("if v != nil {")
+		g.P("enc.AddString(", keyFmt, ", v.AsDuration().String())")
+		g.P("} else {")
+		g.P("enc.AddReflected(", keyFmt, ", nil)")
+		g.P("}")
+	case wktStruct, wktValue, wktListValue, wktAny:
+		// Use AddReflected for complex types
+		g.P("enc.AddReflected(", keyFmt, ", v)")
+	case wktFieldMask:
+		// FieldMask -> comma-separated paths
+		g.P("if v != nil {")
+		g.P("enc.AddString(", keyFmt, ", ", g.QualifiedGoIdent(stringsPkg.Ident("Join")), "(v.GetPaths(), \",\"))")
+		g.P("} else {")
+		g.P("enc.AddReflected(", keyFmt, ", nil)")
+		g.P("}")
+	case wktEmpty:
+		// Empty -> empty object representation
+		g.P("enc.AddReflected(", keyFmt, ", struct{}{})")
+	case wktBoolValue:
+		g.P("if v != nil {")
+		g.P("enc.AddBool(", keyFmt, ", v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AddReflected(", keyFmt, ", nil)")
+		g.P("}")
+	case wktStringValue:
+		g.P("if v != nil {")
+		g.P("enc.AddString(", keyFmt, ", v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AddReflected(", keyFmt, ", nil)")
+		g.P("}")
+	case wktBytesValue:
+		g.P("if v != nil {")
+		g.P("enc.AddBinary(", keyFmt, ", v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AddReflected(", keyFmt, ", nil)")
+		g.P("}")
+	case wktInt32Value:
+		g.P("if v != nil {")
+		g.P("enc.AddInt32(", keyFmt, ", v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AddReflected(", keyFmt, ", nil)")
+		g.P("}")
+	case wktInt64Value:
+		g.P("if v != nil {")
+		g.P("enc.AddInt64(", keyFmt, ", v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AddReflected(", keyFmt, ", nil)")
+		g.P("}")
+	case wktUInt32Value:
+		g.P("if v != nil {")
+		g.P("enc.AddUint32(", keyFmt, ", v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AddReflected(", keyFmt, ", nil)")
+		g.P("}")
+	case wktUInt64Value:
+		g.P("if v != nil {")
+		g.P("enc.AddUint64(", keyFmt, ", v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AddReflected(", keyFmt, ", nil)")
+		g.P("}")
+	case wktFloatValue:
+		g.P("if v != nil {")
+		g.P("enc.AddFloat32(", keyFmt, ", v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AddReflected(", keyFmt, ", nil)")
+		g.P("}")
+	case wktDoubleValue:
+		g.P("if v != nil {")
+		g.P("enc.AddFloat64(", keyFmt, ", v.GetValue())")
+		g.P("} else {")
+		g.P("enc.AddReflected(", keyFmt, ", nil)")
+		g.P("}")
+	default:
+		// Regular message - check for ObjectMarshaler interface
+		g.P("if obj, ok := interface{}(v).(", g.QualifiedGoIdent(zapcorePkg.Ident("ObjectMarshaler")), "); ok {")
+		g.P("enc.AddObject(", keyFmt, ", obj)")
+		g.P("} else {")
+		g.P("enc.AddReflected(", keyFmt, ", v)")
+		g.P("}")
+	}
 }
 
 func generatePrimitiveField(g *protogen.GeneratedFile, f *protogen.Field) {
@@ -129,15 +431,85 @@ func generatePrimitiveField(g *protogen.GeneratedFile, f *protogen.Field) {
 	case protoreflect.GroupKind:
 		g.P("enc.AddReflected(\"", fname, "\", x.", gname, ")")
 	case protoreflect.MessageKind:
+		generatePrimitiveWellKnownOrMessage(g, f, fname, gname)
+	case protoreflect.StringKind:
+		g.P("enc.AddString(\"", fname, "\", x.", gname, ")")
+	default:
+		g.P("enc.AddReflected(\"", fname, "\", x.", gname, ")")
+	}
+}
+
+// generatePrimitiveWellKnownOrMessage generates field encoding for message kinds,
+// handling well-known types with their JSON representation
+func generatePrimitiveWellKnownOrMessage(g *protogen.GeneratedFile, f *protogen.Field, fname protoreflect.Name, gname string) {
+	wkt := getWellKnownType(f)
+	switch wkt {
+	case wktTimestamp:
+		// Timestamp -> RFC 3339 string
+		g.P("if x.", gname, " != nil {")
+		g.P("enc.AddString(\"", fname, "\", x.", gname, ".AsTime().Format(\"2006-01-02T15:04:05.999999999Z07:00\"))")
+		g.P("}")
+	case wktDuration:
+		// Duration -> string like "1.5s"
+		g.P("if x.", gname, " != nil {")
+		g.P("enc.AddString(\"", fname, "\", x.", gname, ".AsDuration().String())")
+		g.P("}")
+	case wktStruct, wktValue, wktListValue, wktAny:
+		// Use AddReflected for complex types
+		g.P("enc.AddReflected(\"", fname, "\", x.", gname, ")")
+	case wktFieldMask:
+		// FieldMask -> comma-separated paths
+		g.P("if x.", gname, " != nil {")
+		g.P("enc.AddString(\"", fname, "\", ", g.QualifiedGoIdent(stringsPkg.Ident("Join")), "(x.", gname, ".GetPaths(), \",\"))")
+		g.P("}")
+	case wktEmpty:
+		// Empty -> empty object representation
+		g.P("if x.", gname, " != nil {")
+		g.P("enc.AddReflected(\"", fname, "\", struct{}{})")
+		g.P("}")
+	case wktBoolValue:
+		g.P("if x.", gname, " != nil {")
+		g.P("enc.AddBool(\"", fname, "\", x.", gname, ".GetValue())")
+		g.P("}")
+	case wktStringValue:
+		g.P("if x.", gname, " != nil {")
+		g.P("enc.AddString(\"", fname, "\", x.", gname, ".GetValue())")
+		g.P("}")
+	case wktBytesValue:
+		g.P("if x.", gname, " != nil {")
+		g.P("enc.AddBinary(\"", fname, "\", x.", gname, ".GetValue())")
+		g.P("}")
+	case wktInt32Value:
+		g.P("if x.", gname, " != nil {")
+		g.P("enc.AddInt32(\"", fname, "\", x.", gname, ".GetValue())")
+		g.P("}")
+	case wktInt64Value:
+		g.P("if x.", gname, " != nil {")
+		g.P("enc.AddInt64(\"", fname, "\", x.", gname, ".GetValue())")
+		g.P("}")
+	case wktUInt32Value:
+		g.P("if x.", gname, " != nil {")
+		g.P("enc.AddUint32(\"", fname, "\", x.", gname, ".GetValue())")
+		g.P("}")
+	case wktUInt64Value:
+		g.P("if x.", gname, " != nil {")
+		g.P("enc.AddUint64(\"", fname, "\", x.", gname, ".GetValue())")
+		g.P("}")
+	case wktFloatValue:
+		g.P("if x.", gname, " != nil {")
+		g.P("enc.AddFloat32(\"", fname, "\", x.", gname, ".GetValue())")
+		g.P("}")
+	case wktDoubleValue:
+		g.P("if x.", gname, " != nil {")
+		g.P("enc.AddFloat64(\"", fname, "\", x.", gname, ".GetValue())")
+		g.P("}")
+	default:
+		// Regular message - check for ObjectMarshaler interface
 		g.P("if obj, ok := interface{}(x.", gname, ").(", g.QualifiedGoIdent(zapcorePkg.Ident("ObjectMarshaler")), "); ok {")
 		g.P("enc.AddObject(\"", fname, "\", obj)")
 		g.P("} else {")
 		g.P("enc.AddReflected(\"", fname, "\", x.", gname, ")")
 		g.P("}")
-	case protoreflect.StringKind:
-		g.P("enc.AddString(\"", fname, "\", x.", gname, ")")
-	default:
-		g.P("enc.AddReflected(\"", fname, "\", x.", gname, ")")
 	}
 }
 
--- test/types/types_test.go
+++ test/types/types_test.go
@@ -2,13 +2,25 @@ package types
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap/zapcore"
+	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/durationpb"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
 	"google.golang.org/protobuf/types/known/structpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 func TestTypes_MarshalLogObject(t *testing.T) {
+	testTime := time.Date(2024, 1, 15, 10, 30, 0, 123456789, time.UTC)
+	testDuration := 90*time.Second + 500*time.Millisecond
+
+	anyVal, _ := anypb.New(&OtherType3{Val: "any_value"})
+
 	m := &Types{
 		SecretVal:   "secret",
 		DoubleVal:   0.1,
@@ -77,6 +89,51 @@ func TestTypes_MarshalLogObject(t *testing.T) {
 				},
 			},
 		},
+		// Well-known types
+		TimestampVal: timestamppb.New(testTime),
+		DurationVal:  durationpb.New(testDuration),
+		AnyVal:       anyVal,
+		FieldMaskVal: &fieldmaskpb.FieldMask{Paths: []string{"field1", "field2", "nested.field"}},
+		EmptyVal:     &emptypb.Empty{},
+		ValueVal: &structpb.Value{
+			Kind: &structpb.Value_StringValue{StringValue: "value_string"},
+		},
+		ListValueVal: &structpb.ListValue{
+			Values: []*structpb.Value{
+				{Kind: &structpb.Value_StringValue{StringValue: "item1"}},
+				{Kind: &structpb.Value_NumberValue{NumberValue: 42}},
+			},
+		},
+		// Wrapper types
+		BoolWrapperVal:   wrapperspb.Bool(true),
+		StringWrapperVal: wrapperspb.String("wrapped_string"),
+		BytesWrapperVal:  wrapperspb.Bytes([]byte{4, 5, 6}),
+		Int32WrapperVal:  wrapperspb.Int32(100),
+		Int64WrapperVal:  wrapperspb.Int64(200),
+		Uint32WrapperVal: wrapperspb.UInt32(300),
+		Uint64WrapperVal: wrapperspb.UInt64(400),
+		FloatWrapperVal:  wrapperspb.Float(1.5),
+		DoubleWrapperVal: wrapperspb.Double(2.5),
+		// Repeated well-known types
+		RepeatedTimestampVal: []*timestamppb.Timestamp{
+			timestamppb.New(testTime),
+			timestamppb.New(testTime.Add(time.Hour)),
+		},
+		RepeatedDurationVal: []*durationpb.Duration{
+			durationpb.New(time.Minute),
+			durationpb.New(time.Hour),
+		},
+		RepeatedStringWrapperVal: []*wrapperspb.StringValue{
+			wrapperspb.String("wrapped1"),
+			wrapperspb.String("wrapped2"),
+		},
+		// Map with well-known types
+		MapTimestampVal: map[string]*timestamppb.Timestamp{
+			"time1": timestamppb.New(testTime),
+		},
+		MapStringWrapperVal: map[string]*wrapperspb.StringValue{
+			"key1": wrapperspb.String("map_wrapped_string"),
+		},
 		XString:                   "foo",
 		OptionalVal:               refs("foo"),
 		OptionalNotPresentVal:     nil,
@@ -92,80 +149,116 @@ func TestTypes_MarshalLogObject(t *testing.T) {
 	if !assert.NoError(t, err) {
 		return
 	}
+
+	// Check non-JSON fields with exact matching
+	assert.Equal(t, "[MASKED]", enc.Fields["secret_val"])
+	assert.Equal(t, "[MASKED]", enc.Fields["secret_val2"])
+	assert.Equal(t, float64(0.1), enc.Fields["double_val"])
+	assert.Equal(t, float32(0.1), enc.Fields["float_val"])
+	assert.Equal(t, int32(1), enc.Fields["int32_val"])
+	assert.Equal(t, int64(1), enc.Fields["int64_val"])
+	assert.Equal(t, uint32(1), enc.Fields["uint32_val"])
+	assert.Equal(t, uint64(1), enc.Fields["uint64_val"])
+	assert.Equal(t, int32(2), enc.Fields["sint32_val"])
+	assert.Equal(t, int64(2), enc.Fields["sint64_val"])
+	assert.Equal(t, uint32(2), enc.Fields["fixed32_val"])
+	assert.Equal(t, uint64(2), enc.Fields["fixed64_val"])
+	assert.Equal(t, int32(3), enc.Fields["sfixed32_val"])
+	assert.Equal(t, int64(3), enc.Fields["sfixed64_val"])
+	assert.Equal(t, true, enc.Fields["bool_val"])
+	assert.Equal(t, "string", enc.Fields["string_val"])
+	assert.Equal(t, []byte{1, 2, 3}, enc.Fields["bytes_val"])
+	assert.Equal(t, Types_ENUM_1.String(), enc.Fields["enum_val"])
+
+	// Nested message types
 	assert.EqualValues(t, map[string]interface{}{
-		"secret_val":   "[MASKED]",
-		"secret_val2":  "[MASKED]",
-		"double_val":   float64(0.1),
-		"float_val":    float32(0.1),
-		"int32_val":    int32(1),
-		"int64_val":    int64(1),
-		"uint32_val":   uint32(1),
-		"uint64_val":   uint64(1),
-		"sint32_val":   int32(2),
-		"sint64_val":   int64(2),
-		"fixed32_val":  uint32(2),
-		"fixed64_val":  uint64(2),
-		"sfixed32_val": int32(3),
-		"sfixed64_val": int64(3),
-		"bool_val":     true,
-		"string_val":   "string",
-		"bytes_val":    []byte{1, 2, 3},
-		"enum_val":     Types_ENUM_1.String(),
-		"other_type_val": map[string]interface{}{
+		"other_string_val": "other_string",
+		"other_secret_val": "[MASKED]",
+	}, enc.Fields["other_type_val"])
+	assert.EqualValues(t, map[string]interface{}{
+		"nested_string_val": "nested_string",
+		"nested_secret_val": "[MASKED]",
+	}, enc.Fields["nested_type_val"])
+	assert.EqualValues(t, map[string]interface{}{
+		"nested_string_val": "other_nested_string",
+		"nested_secret_val": "[MASKED]",
+	}, enc.Fields["other_type_nested_type_val"])
+
+	// Oneof
+	assert.Equal(t, "", enc.Fields["oneof_string_val"])
+
+	// Maps
+	assert.EqualValues(t, map[string]interface{}{"foo": "bar"}, enc.Fields["map_val1"])
+	assert.EqualValues(t, map[string]interface{}{
+		"foo": map[string]interface{}{
 			"other_string_val": "other_string",
 			"other_secret_val": "[MASKED]",
 		},
-		"nested_type_val": map[string]interface{}{
-			"nested_string_val": "nested_string",
-			"nested_secret_val": "[MASKED]",
+	}, enc.Fields["map_val2"])
+	assert.EqualValues(t, map[string]interface{}{}, enc.Fields["map_empty_val"])
+
+	// Repeated
+	assert.EqualValues(t, []interface{}{"foo", "bar"}, enc.Fields["repeated_val1"])
+	assert.EqualValues(t, []interface{}{Types_ENUM_1.String(), Types_ENUM_2.String()}, enc.Fields["repeated_val2"])
+	assert.EqualValues(t, []interface{}{
+		map[string]interface{}{
+			"other_string_val": "other_string",
+			"other_secret_val": "[MASKED]",
 		},
-		"other_type_nested_type_val": map[string]interface{}{
-			"nested_string_val": "other_nested_string",
-			"nested_secret_val": "[MASKED]",
-		},
-		"oneof_string_val": "",
-		"map_val1": map[string]interface{}{
-			"foo": "bar",
-		},
-		"map_val2": map[string]interface{}{
-			"foo": map[string]interface{}{
-				"other_string_val": "other_string",
-				"other_secret_val": "[MASKED]",
-			},
-		},
-		"map_empty_val": map[string]interface{}{},
-		"repeated_val1": []interface{}{"foo", "bar"},
-		"repeated_val2": []interface{}{Types_ENUM_1.String(), Types_ENUM_2.String()},
-		"repeated_val3": []interface{}{
-			map[string]interface{}{
-				"other_string_val": "other_string",
-				"other_secret_val": "[MASKED]",
-			},
-		},
-		"repeated_empty_val": []interface{}{},
-		"struct_val": &structpb.Struct{
-			Fields: map[string]*structpb.Value{
-				"struct_string": {
-					Kind: &structpb.Value_StringValue{StringValue: "foo"},
-				},
-				"struct_number": {
-					Kind: &structpb.Value_NumberValue{NumberValue: 0.1},
-				},
-				"struct_bool": {
-					Kind: &structpb.Value_BoolValue{BoolValue: true},
-				},
-			},
-		},
-		"_String":       "foo",
-		"optional_val":  "foo",
-		"optional_enum": Types_ENUM_1.String(),
-		"optional_message": map[string]interface{}{
-			"val": "foo",
-		},
-		"present_message": map[string]interface{}{
-			"val": "foo",
-		},
-	}, enc.Fields)
+	}, enc.Fields["repeated_val3"])
+	assert.EqualValues(t, []interface{}{}, enc.Fields["repeated_empty_val"])
+
+	// Well-known types serialized with AddReflected - compare actual protobuf objects
+	assert.Equal(t, m.StructVal, enc.Fields["struct_val"])
+	assert.Equal(t, m.AnyVal, enc.Fields["any_val"])
+	assert.Equal(t, m.ValueVal, enc.Fields["value_val"])
+	assert.Equal(t, m.ListValueVal, enc.Fields["list_value_val"])
+
+	// Well-known types with native representations
+	assert.Equal(t, testTime.Format("2006-01-02T15:04:05.999999999Z07:00"), enc.Fields["timestamp_val"])
+	assert.Equal(t, testDuration.String(), enc.Fields["duration_val"])
+	assert.Equal(t, "field1,field2,nested.field", enc.Fields["field_mask_val"])
+	assert.Equal(t, struct{}{}, enc.Fields["empty_val"])
+
+	// Wrapper types - unwrapped to native types
+	assert.Equal(t, true, enc.Fields["bool_wrapper_val"])
+	assert.Equal(t, "wrapped_string", enc.Fields["string_wrapper_val"])
+	assert.Equal(t, []byte{4, 5, 6}, enc.Fields["bytes_wrapper_val"])
+	assert.Equal(t, int32(100), enc.Fields["int32_wrapper_val"])
+	assert.Equal(t, int64(200), enc.Fields["int64_wrapper_val"])
+	assert.Equal(t, uint32(300), enc.Fields["uint32_wrapper_val"])
+	assert.Equal(t, uint64(400), enc.Fields["uint64_wrapper_val"])
+	assert.Equal(t, float32(1.5), enc.Fields["float_wrapper_val"])
+	assert.Equal(t, float64(2.5), enc.Fields["double_wrapper_val"])
+
+	// Repeated well-known types
+	assert.EqualValues(t, []interface{}{
+		testTime.Format("2006-01-02T15:04:05.999999999Z07:00"),
+		testTime.Add(time.Hour).Format("2006-01-02T15:04:05.999999999Z07:00"),
+	}, enc.Fields["repeated_timestamp_val"])
+	assert.EqualValues(t, []interface{}{
+		time.Minute.String(),
+		time.Hour.String(),
+	}, enc.Fields["repeated_duration_val"])
+	assert.EqualValues(t, []interface{}{
+		"wrapped1",
+		"wrapped2",
+	}, enc.Fields["repeated_string_wrapper_val"])
+
+	// Map with well-known types
+	assert.EqualValues(t, map[string]interface{}{
+		"time1": testTime.Format("2006-01-02T15:04:05.999999999Z07:00"),
+	}, enc.Fields["map_timestamp_val"])
+	assert.EqualValues(t, map[string]interface{}{
+		"key1": "map_wrapped_string",
+	}, enc.Fields["map_string_wrapper_val"])
+
+	// Other fields
+	assert.Equal(t, "foo", enc.Fields["_String"])
+	assert.Equal(t, "foo", enc.Fields["optional_val"])
+	assert.Equal(t, Types_ENUM_1.String(), enc.Fields["optional_enum"])
+	assert.EqualValues(t, map[string]interface{}{"val": "foo"}, enc.Fields["optional_message"])
+	assert.EqualValues(t, map[string]interface{}{"val": "foo"}, enc.Fields["present_message"])
 
 	assert.NotContains(t, enc.Fields, "oneof_int64_val")
 	assert.NotContains(t, enc.Fields, "oneof_bool_val")
